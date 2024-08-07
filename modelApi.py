db13e58ef4e8b1f498b9d0665df447597052d11a
ifurkanatasoy <furkann.atasoy@gmail.com>
2024-08-07 18:18:12 +0300

    change

--- modelApi.py
+++ modelApi.py
@@ -2,12 +2,12 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
 import uvicorn
 from typing import List, Dict
-# import torch
-# from mini_pyabsa import AspectSentimentTripletExtraction as ASTE
+import torch
+from mini_pyabsa import AspectSentimentTripletExtraction as ASTE
 
-# checkpoint_path = r'checkpoints\dataset_45.31'
+checkpoint_path = r'checkpoints\dataset_45.31'
 
-# model = ASTE.AspectSentimentTripletExtractor(checkpoint_path)
+model = ASTE.AspectSentimentTripletExtractor(checkpoint_path)
 
 app = FastAPI()
 
@@ -21,38 +21,25 @@ class PredictionResult(BaseModel):
 @app.post("/predict/", response_model=PredictionResult)
 async def predict(item: Item):
     
-    # input_text = item.text
+    input_text = item.text
     
-    # with torch.no_grad():
-        # entity_list, results = model.predict(input_text)  
+    with torch.no_grad():
+        entity_list, results = model.predict(input_text)  
 
-        # formatted_result = {
-        #     "entity_list": entity_list,
-        #     "results": [
-        #         {
-        #             "entity": entity,
-        #             "sentiment": sentiment
-        #         }
-        #         for result_dict in results
-        #         for entity, sentiment in result_dict.items()
-        #     ]
-        # }
-
-    # return formatted_result
-
-    result = {
-        "entity_list": [
-            "entities"
-        ],
-        "results": [
-            {
-            "entity": "entities",
-            "sentiment": "sentiments"
-            }
-        ]
+        formatted_result = {
+            "entity_list": entity_list,
+            "results": [
+                {
+                    "entity": entity,
+                    "sentiment": sentiment
+                }
+                for result_dict in results
+                for entity, sentiment in result_dict.items()
+            ]
         }
 
-    return result
+    return formatted_result
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
